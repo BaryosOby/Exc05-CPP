a7dd787452216ec520a500dbc0c4119271b944c5
BaryosOby <omribaryos@gmail.com>
2022-01-14 16:13:19 +0200

    second commit

--- Element.h
+++ Element.h
@@ -14,52 +14,54 @@ private:
 
 public:
     Element():x(0),fx(0),rank(0){};
-    Element(vector<T> xvect, vector<E> fx_vect): x(xvect), fx(fx_vect), rank(0) {};
+    Element(vector<T>& xvect, vector<E>& fx_vect): x(xvect), fx(fx_vect), rank(0) {};
     vector<T>& getX() const{ return x;}
-    void setX(vector<T>& x) {this->x = x;}
+//    void setX(vector<T>& x) {this->x = x;}
     vector<E>& getFx() const { return fx;}
-    void setfX(vector<T>& f) const{this->fx = fx;}
+//    void setfX(vector<T>& f) const{this->fx = fx;}
 
-    void setRank(int r) { rank = r;}
+//    void setRank(int r) { rank = r;}
+
+    Element& operator++(){
+        rank++;
+        return *this;
+    }
+
+    Element& operator++(int){
+        ++rank;
+        return *this;
+    }
+
     int getRank() const { return rank;}
+
     int findDominate(Element& other){
         int count = 0;
         int countOther=0;
         int big = 0;
-        for (int i =0 ;i<fx.size(); i++){
-            if (fx[i] >= other.fx[i]){
+        for (int i = 0 ;i<fx.size(); i++){
+            if (fx[i] <= other.fx[i]){
                 count++;
             }
-            if (fx[i] > other.fx[i]){
+            if (fx[i] < other.fx[i]){
                 big =1 ;
             }
-            if (fx[i]<=other.fx[i]){
+            if (fx[i] >= other.fx[i]){
                 countOther++;
             }
-            if (fx[i] < other.fx[i])
+            if (fx[i] > other.fx[i])
             {
                 big =-1;
             }
         }
-        if (big == 1 && count == fx.size()){return 1;}
-        if (big == -1 && countOther == fx.size()){return -1;}
+        if (big == 1 && count == fx.size()){ return 1; }
+        if (big == -1 && countOther == fx.size()){ return -1; }
         return 0;
     }
-    Element operator+(const Element& e) const
-    {
-        return Element(x + e.getX(),0);
-    }
-//
-//
-//    // copy c'tor
-//
-//    Element& operator=(Element other)
+//    Element operator+(const Element& e) const
 //    {
-//        swap(x, other.x);
-//        swap(fx, other.fx);
-//        swap(rank, other.rank);
-//        return *this;
+//        return Element(x + e.getX(),0);
 //    }
+
 };
 
 
--- ElementContainer.h
+++ ElementContainer.h
@@ -10,17 +10,59 @@ private:
     int vectorSize;
     int fxSize;
 
-
-
     void paretoRanking(){
         for(int i = 0 ; i< size; i++){
             for (int j= i+1 ; j<size; j++){
-
+                int dominanceRes = elements[i].findDominate(elements[j]);
+                if(dominanceRes > 0){
+                    elements[j]++;
+                }
+                if(dominanceRes < 0){
+                    elements[i]++;
+                }
             }
         }
     }
 
+    void merge(int start, int mid, int end){
+        vector<Element<T, E>> temp;
+        int i = start, j = mid + 1;
+        while(i <= mid && j <= end){
+            if(elements[i].getRank() <= elements[j].getRank()){
+                temp.push_back(elements[i]);
+                i++;
+            }
+            else{
+                temp.push_back(elements[j]);
+                j++;
+            }
+        }
+        while(i <= mid){
+            temp.push_back(elements[i]);
+            i++;
+        }
+        while(j <= end){
+            temp.push_back(elements[j]);
+            j++;
+        }
+        for(int k = start; k <= end; k++){
+            elements[k] = temp[k - start];
+        }
+
+    }
+
+    void ElementsReordering(int start, int end){
+        // using Merge sort by rank
+        if(start < end){
+            int mid = (start+end) / 2;
+            ElementsReordering(start, mid);
+            ElementsReordering(mid+1, end);
+            merge(start, mid, end);
+        }
+    }
+
 public:
+    ElementContainer(): elements(0), size(0), vectorSize(0), fxSize(0){}
     ElementContainer(vector<Element<T, E>>& elems, int mu, int n, int m) : elements(elems), size(mu), vectorSize(n), fxSize(m){}
 
     void addElements(vector<Element<T, E>>& vect){
@@ -29,6 +71,11 @@ public:
 
     void removeKElements(int k){
         elements.erase(elements.begin()+k,elements.end());
+    }
+
+    void pareSorting(){
+        paretoRanking();
+        ElementsReordering(0, size);
     }
 
 };
